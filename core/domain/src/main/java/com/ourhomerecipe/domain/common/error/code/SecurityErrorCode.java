87a5f322a2bf02613c4f7d00aa217dd1f07e88a1
elrdan <seongo9655@gmail.com>
2024-09-07 08:20:27 +0900

    feat: jwt security error code를 관리하는 enum 추가

---
+++ core/domain/src/main/java/com/ourhomerecipe/domain/common/error/code/SecurityErrorCode.java
@@ -0,0 +1,30 @@
+package com.ourhomerecipe.domain.common.error.code;
+
+import org.springframework.http.HttpStatus;
+
+import com.ourhomerecipe.domain.common.error.response.ErrorResponse;
+
+import lombok.Getter;
+
+@Getter
+public enum SecurityErrorCode implements BaseErrorCode {
+	VALIDATION_TOKEN_FAILED(401, "정상적인 토큰이 아닙니다. 확인 후 다시 시도해주세요.", HttpStatus.UNAUTHORIZED),
+	VALIDATION_NOT_EXISTS_TOKEN_FAILED(401, "토큰이 존재하지 않습니다.", HttpStatus.UNAUTHORIZED),
+	VALIDATION_TOKEN_EXPIRED(401, "토큰의 유효기한이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
+	VALIDATION_TOKEN_NOT_AUTHORIZATION(403, "접근 권한이 없습니다.", HttpStatus.FORBIDDEN);
+
+	private final int errorCode;
+	private final String errorMessage;
+	private final HttpStatus status;
+
+	SecurityErrorCode(int errorCode, String message, HttpStatus status) {
+		this.errorCode = errorCode;
+		this.errorMessage = message;
+		this.status = status;
+	}
+
+	@Override
+	public ErrorResponse getErrorResponse() {
+		return new ErrorResponse(this.errorCode, this.errorMessage);
+	}
+}
