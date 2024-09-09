cdebb8da11b65e1b68b4ecb0014057bdab0c6210
elrdan <seongo9655@gmail.com>
2024-09-09 21:03:23 +0900

    feat: 회원 관리를 위한 MemberRepository 파일 추가

---
+++ core/domain/src/main/java/com/ourhomerecipe/domain/member/repository/MemberRepositoryImpl.java
@@ -0,0 +1,11 @@
+package com.ourhomerecipe.domain.member.repository;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepositoryImpl implements MemberCustom{
+
+}
