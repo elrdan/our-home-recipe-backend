9773425cb379aa5968297a89f56c0d265d643764
elrdan <seongo9655@gmail.com>
2024-09-28 02:08:21 +0900

    feat: CustomSecurityException handler 추가

--- api/member/src/main/java/com/ourhomerecipe/member/exception/GlobalExceptionHandler.java
+++ api/member/src/main/java/com/ourhomerecipe/member/exception/GlobalExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.ourhomerecipe.domain.common.error.code.BaseErrorCode;
 import com.ourhomerecipe.domain.common.error.response.ErrorResponse;
 import com.ourhomerecipe.domain.common.exception.CustomRedisException;
+import com.ourhomerecipe.security.exception.CustomSecurityException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -38,6 +39,14 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(CustomRedisException.class)
 	protected ResponseEntity<ErrorResponse> handleRedisException(CustomRedisException ex) {
 		log.error(">>>>> RedisException : {}", ex);
+		BaseErrorCode errorCode = ex.getErrorCode();
+		return ResponseEntity.status(errorCode.getStatus())
+			.body(errorCode.getErrorResponse());
+	}
+
+	@ExceptionHandler(CustomSecurityException.class)
+	protected ResponseEntity<ErrorResponse> handleSecurityException(CustomSecurityException ex) {
+		log.error(">>>>> SecurityException : {}", ex);
 		BaseErrorCode errorCode = ex.getErrorCode();
 		return ResponseEntity.status(errorCode.getStatus())
 			.body(errorCode.getErrorResponse());
